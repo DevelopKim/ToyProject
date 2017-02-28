88731ea9ea3c353860b2c5401726409aaa46f38b
Shinwoo Moon <chocoma87@gmail.com>
2017-02-28 11:31:43 +0900

    Set basic webpack and react setting

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+
+    entry: path.resolve(__dirname, 'public') + '/javascripts/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public') + '/dist',
+        filename: 'bundle.js',
+        // 번들된 파일의 루트 경로 (/public/dist/bundle.js 가 아니고, /dist/bundle.js 이다.)
+        publicPath: '/dist/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+};
